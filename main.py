e0c88c2256d522a3457383931ce87d24668a14ed
talha-mahmood <talhamahmood1261@gmail.com>
2024-02-06 21:46:37 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
+
+
+hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+
+engine = create_engine("postgresql://talhamahmood1261:PDZYAQwI75Ok@ep-noisy-meadow-972632.ap-southeast-1.aws.neon.tech/sqlmodel?sslmode=require")
+
+
+SQLModel.metadata.create_all(engine)
+
+with Session(engine) as session:
+    session.add(hero_1)
+    session.add(hero_2)
+    session.add(hero_3)
+    session.commit()
